e4f0606f0c1630252c809af473e8764b874949bd
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-06-26 15:39:24 +0300

    Add files via upload

---
+++ ketp.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ketp
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(51, 64)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(182, 125)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.TextBox8)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(-15, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(655, 374)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(392, 2234)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Label5"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label4.Location = New System.Drawing.Point(29, 298)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 22)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Fodulus key"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(30, 323)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(533, 20)
+        Me.TextBox8.TabIndex = 8
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(27, 8)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(534, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "paneli kapat"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label3.Location = New System.Drawing.Point(356, 39)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 22)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Gizli anahtar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label2.Location = New System.Drawing.Point(47, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(178, 22)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Herkese açik anahtar"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label1.Location = New System.Drawing.Point(27, 4555)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(229, 260)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(139, 59)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Anahtarlari oluştur"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(360, 64)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(182, 125)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(54, 130)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(212, 68)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "şifrele"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(54, 224)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(54, 11)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(212, 113)
+        Me.TextBox4.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(292, 12)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(212, 113)
+        Me.TextBox5.TabIndex = 6
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(292, 224)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox6.TabIndex = 5
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(292, 131)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(212, 68)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "çöz"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(292, 267)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(212, 20)
+        Me.TextBox7.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label6.Location = New System.Drawing.Point(11, 268)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(107, 22)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Fodulus key"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(12, 293)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(533, 20)
+        Me.TextBox9.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(488, 2089)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(292, 206)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(68, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Açık Anahtar"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(51, 208)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(68, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Açık Anahtar"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(292, 251)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(66, 13)
+        Me.Label10.TabIndex = 15
+        Me.Label10.Text = "Gizli Anahtar"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 319)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(534, 27)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "anahtar oluşturma paneli"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ketp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(558, 361)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.KeyPreview = True
+        Me.Name = "ketp"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "KEP Algorithm"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button6 As Button
+End Class
---
+++ ketp.vb
@@ -0,0 +1,228 @@
+﻿Public Class ketp
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = acikanahtar()
+        TextBox2.Text = gizlianahtar(TextBox1.Text, Label1.Text)
+    End Sub
+    'for döngülerini kendinize göre ayarlayin ve fobulus keyi(isteğe bağli) de öyle ayarlayin. yoksa gizli anahtariniz hemen hackerin eline geçer.
+    'Adjust the for loops according to you and adjust the fobulus key(is optional) as well. Otherwise, your secret key will fall into the hands of the hacker immediately.
+    Public Function sifrele(ByVal acikkey As String, ByVal sifrelenecekbayt As Byte())
+        If TextBox9.Text = Nothing Then
+            Try
+                Dim hıohaha As New System.Security.Cryptography.RijndaelManaged
+                Dim hıohahaes As New System.Security.Cryptography.AesCryptoServiceProvider
+                hıohaha.KeySize = 256
+                hıohaha.BlockSize = 256
+                hıohahaes.KeySize = 256
+                hıohahaes.BlockSize = 128
+                hıohaha.Mode = System.Security.Cryptography.CipherMode.ECB
+                hıohahaes.Mode = System.Security.Cryptography.CipherMode.ECB
+                hıohaha.Padding = Security.Cryptography.PaddingMode.PKCS7
+                hıohahaes.Padding = Security.Cryptography.PaddingMode.PKCS7
+                Dim wqho As New System.Security.Cryptography.SHA256CryptoServiceProvider
+                Dim w As New System.Security.Cryptography.MD5CryptoServiceProvider
+                Dim a2q = System.Text.Encoding.UTF8.GetBytes(acikkey)
+                Dim aq18 = w.ComputeHash(a2q)
+                hıohahaes.Key = aq18
+                Dim sifrele1 As System.Security.Cryptography.ICryptoTransform = hıohahaes.CreateEncryptor
+                Dim ahoh = sifrele1.TransformFinalBlock(sifrelenecekbayt, 0, sifrelenecekbayt.Length)
+                Dim q12 As String = ""
+                Dim q As String = ""
+                Dim hhw As Integer = 0
+                q = sha512(acikkey)
+                For hhw = hhw To 0
+                    q12 = sha512(q)
+                    q = q12
+                Next
+                Dim ewwew = ripemd160(q12)
+                Dim aq123 = System.Text.Encoding.UTF8.GetBytes(ewwew)
+                Dim aq81 = wqho.ComputeHash(aq123)
+                hıohaha.Key = aq81
+                Dim sifrele2 As System.Security.Cryptography.ICryptoTransform = hıohaha.CreateEncryptor
+                Dim ahoh2 = sifrele2.TransformFinalBlock(ahoh, 0, ahoh.Length)
+                Return ahoh2
+            Catch Ex As Exception
+                MsgBox("Bir hata oluştu! Hata = " & Ex.Message, MsgBoxStyle.Critical, "KEP Algorithm")
+            End Try
+        Else
+            Try
+                Dim hıohaha As New System.Security.Cryptography.RijndaelManaged
+                Dim hıohahaes As New System.Security.Cryptography.AesCryptoServiceProvider
+                hıohaha.KeySize = 256
+                hıohaha.BlockSize = 256
+                hıohahaes.KeySize = 256
+                hıohahaes.BlockSize = 128
+                hıohaha.Mode = System.Security.Cryptography.CipherMode.ECB
+                hıohahaes.Mode = System.Security.Cryptography.CipherMode.ECB
+                hıohaha.Padding = Security.Cryptography.PaddingMode.PKCS7
+                hıohahaes.Padding = Security.Cryptography.PaddingMode.PKCS7
+                Dim wqho As New System.Security.Cryptography.SHA256CryptoServiceProvider
+                Dim w As New System.Security.Cryptography.MD5CryptoServiceProvider
+                Dim a2q = System.Text.Encoding.UTF8.GetBytes(acikkey)
+                Dim aq18 = w.ComputeHash(a2q)
+                hıohahaes.Key = aq18
+                Dim sifrele1 As System.Security.Cryptography.ICryptoTransform = hıohahaes.CreateEncryptor
+                Dim ahoh = sifrele1.TransformFinalBlock(sifrelenecekbayt, 0, sifrelenecekbayt.Length)
+                Dim q12 As String = ""
+                Dim q As String = ""
+                Dim hhw As Integer = 0
+                q = sha512(acikkey)
+                For hhw = hhw To 0
+                    q12 = sha512(q)
+                    q = q12
+                Next
+                q12 = q12 & TextBox9.Text
+                Dim ewwew = ripemd160(q12)
+                Dim aq123 = System.Text.Encoding.UTF8.GetBytes(ewwew)
+                Dim aq81 = wqho.ComputeHash(aq123)
+                hıohaha.Key = aq81
+                Dim sifrele2 As System.Security.Cryptography.ICryptoTransform = hıohaha.CreateEncryptor
+                Dim ahoh2 = sifrele2.TransformFinalBlock(ahoh, 0, ahoh.Length)
+                Return ahoh2
+            Catch Ex As Exception
+                MsgBox("Bir hata oluştu! Hata = " & Ex.Message, MsgBoxStyle.Critical, "KEP Algorithm")
+            End Try
+        End If
+    End Function
+    Public Function coz(ByVal acikkey As String, ByVal gizlikey As String, ByVal cozulecekbayt As Byte())
+        Try
+            Dim hıohaha As New System.Security.Cryptography.RijndaelManaged
+            Dim hıohahaes As New System.Security.Cryptography.AesCryptoServiceProvider
+            hıohaha.KeySize = 256
+            hıohaha.BlockSize = 256
+            hıohahaes.KeySize = 256
+            hıohahaes.BlockSize = 128
+            hıohaha.Mode = System.Security.Cryptography.CipherMode.ECB
+            hıohahaes.Mode = System.Security.Cryptography.CipherMode.ECB
+            hıohaha.Padding = Security.Cryptography.PaddingMode.PKCS7
+            hıohahaes.Padding = Security.Cryptography.PaddingMode.PKCS7
+            Dim wqho As New System.Security.Cryptography.SHA256CryptoServiceProvider
+            Dim w As New System.Security.Cryptography.MD5CryptoServiceProvider
+            Dim a3q = System.Text.Encoding.UTF8.GetBytes(gizlikey)
+            Dim k13 = wqho.ComputeHash(a3q)
+            hıohaha.Key = k13
+            Dim sifrele2 As System.Security.Cryptography.ICryptoTransform = hıohaha.CreateDecryptor
+            Dim aho2 = sifrele2.TransformFinalBlock(cozulecekbayt, 0, cozulecekbayt.Length)
+            Dim a2q = System.Text.Encoding.UTF8.GetBytes(acikkey)
+            Dim aq18 = w.ComputeHash(a2q)
+            hıohahaes.Key = aq18
+            Dim sifrele1 As System.Security.Cryptography.ICryptoTransform = hıohahaes.CreateDecryptor
+            Dim ahoh = sifrele1.TransformFinalBlock(aho2, 0, aho2.Length)
+            Return ahoh
+        Catch Ex As Exception
+            MsgBox("Bir hata oluştu! Hata = " & Ex.Message, MsgBoxStyle.Critical, "KEP Algorithm")
+        End Try
+    End Function
+    Public Function acikanahtar()
+        Dim hhw As Integer = 0
+        Dim veri1 As Integer
+        Dim veri2 As Char
+        Dim veri3 As Integer
+        Dim rastgele As Random = New Random()
+        Dim whq As String = ""
+        Dim q12 As String = ""
+
+        For hhw = hhw To 200
+            veri1 = rastgele.Next(65, 80)
+            veri3 = rastgele.Next(1, 100)
+            veri2 = Convert.ToChar(veri1)
+            whq += veri2 & veri3
+        Next
+        Return whq.ToLower
+
+    End Function
+    Public Function gizlianahtar(ByVal anwe As String, ByVal q As String)
+        If TextBox8.Text = Nothing Then
+            Dim q12 As String = ""
+            Dim hhw As Integer = 0
+            q = sha512(anwe)
+            For hhw = hhw To 0
+                q12 = sha512(q)
+                q = q12
+            Next
+            Return ripemd160(q12)
+        Else
+            Dim q12 As String = ""
+            Dim hhw As Integer = 0
+            q = sha512(anwe)
+            For hhw = hhw To 0
+                q12 = sha512(q)
+                q = q12
+            Next
+            q12 = q12 & TextBox8.Text
+            Return ripemd160(q12)
+        End If
+
+    End Function
+    Public Function sha512(ByVal anan As String) As String
+        Dim sha221 As New Security.Cryptography.SHA512CryptoServiceProvider
+        Dim baytyz() As Byte = System.Text.Encoding.ASCII.GetBytes(anan)
+        baytyz = sha221.ComputeHash(baytyz)
+        Dim hohaha As String = Nothing
+        For Each byt As Byte In baytyz
+            hohaha &= byt.ToString("x2")
+        Next
+        Return hohaha
+    End Function
+    Public Function ripemd160(ByVal anan As String) As String
+        Dim sha221 As New Security.Cryptography.RIPEMD160Managed
+        Dim baytyz() As Byte = System.Text.Encoding.ASCII.GetBytes(anan)
+        baytyz = sha221.ComputeHash(baytyz)
+        Dim hohaha As String = Nothing
+        For Each byt As Byte In baytyz
+            hohaha &= byt.ToString("x2")
+        Next
+        Return hohaha
+    End Function
+
+    Private Sub ketp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Dim qwe = System.Text.Encoding.UTF8.GetBytes(TextBox4.Text)
+            Dim wqe = sifrele(TextBox3.Text, qwe)
+            TextBox4.Text = Convert.ToBase64String(wqe)
+        Catch ex As Exception
+            MsgBox("Bir hata oluştu! Hata = " & ex.Message, MsgBoxStyle.Critical, "KEP Algorithm")
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Panel1.Visible = False
+        TextBox1.Text = Nothing
+        TextBox2.Text = Nothing
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            Dim wqe = Convert.FromBase64String(TextBox5.Text)
+            Dim weq = coz(TextBox6.Text, TextBox7.Text, wqe)
+            TextBox5.Text = System.Text.Encoding.UTF8.GetString(weq)
+        Catch ex As Exception
+            MsgBox("Bir hata oluştu! Hata = " & ex.Message, MsgBoxStyle.Critical, "KEP Algorithm")
+        End Try
+    End Sub
+
+    Private Sub TextBox8_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox8.KeyPress
+        Label5.Text = Convert.ToInt32(e.KeyChar)
+        If Label5.Text = 32 Then
+            e.Handled = True
+        Else
+
+        End If
+    End Sub
+
+    Private Sub TextBox9_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox9.KeyPress
+        Label7.Text = Convert.ToInt32(e.KeyChar)
+        If Label7.Text = 32 Then
+            e.Handled = True
+        Else
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Panel1.Visible = True
+    End Sub
+End Class
